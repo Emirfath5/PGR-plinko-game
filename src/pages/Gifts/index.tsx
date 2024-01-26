7be9100fff74a16aca82cbedbaa2fff2883fcfb9
Emirfath5 <61555207+Emirfath5@users.noreply.github.com>
2024-01-26 14:27:26 -0500

    Update index.tsx

--- src/pages/Gifts/index.tsx
+++ src/pages/Gifts/index.tsx
@@ -12,28 +12,28 @@ export function Gifts() {
     navigate('/')
   }
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-4 text-text">
-      <Gift className="text-purple" weight="fill" size="80" />
-      <span className="text-center text-lg font-bold">
-        Eba!!! Temos um presente pra você!
-      </span>
-      <span className="text-center text-lg font-bold">
-        Percebemos que você tem somente{' '}
-        <span className="text-purple">{formatPoints(currentBalance)}</span>{' '}
-        pontos.
-        <br />
-        Mas não se preocupe, nesta página você pode resgatar até{' '}
-        <span className="text-purple">300</span> pontos
-        <br /> cada vez que seu saldo estiver abaixo de{' '}
-        <span className="text-purple">10 PPs</span> <br />
-        basta clicar no botão abaixo e contar com a sorte. <br />
-      </span>
-      <button
-        onClick={handleRedeemGift}
-        className="rounded-md bg-purpleDark px-4 py-2 font-bold text-text transition-colors hover:bg-purple"
-      >
-        RESGATAR
-      </button>
-    </div>
+   <div className="flex h-full flex-col items-center justify-center gap-4 text-text">
+  <Gift className="text-purple" weight="fill" size="80" />
+  <span className="text-center text-lg font-bold">
+    Yay!!! We have a gift for you!
+  </span>
+  <span className="text-center text-lg font-bold">
+    We noticed that you only have{' '}
+    <span className="text-purple">{formatPoints(currentBalance)}</span>{' '}
+    points.
+    <br />
+    But don't worry, on this page, you can redeem up to{' '}
+    <span className="text-purple">300</span> points
+    <br /> each time your balance is below{' '}
+    <span className="text-purple">10 PPs</span>. <br />
+    Just click the button below and try your luck. <br />
+  </span>
+  <button
+    onClick={handleRedeemGift}
+    className="rounded-md bg-purpleDark px-4 py-2 font-bold text-text transition-colors hover:bg-purple"
+  >
+    REDEEM
+  </button>
+</div>
   )
 }
