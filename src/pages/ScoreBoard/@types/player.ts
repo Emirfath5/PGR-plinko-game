dcb24e74f5f16a990ff0b3dfbdecbfa35a10725a
Emirfath5 <61555207+Emirfath5@users.noreply.github.com>
2024-01-26 19:08:27 -0500

    Update player.ts

--- src/pages/ScoreBoard/@types/player.ts
+++ src/pages/ScoreBoard/@types/player.ts
@@ -1,6 +1,13 @@
 export interface User {
-  name: string
-  profilePic: string
-  uid: string
-  currentBalance: number
+  uid: string;
+  name: string;
+  email: string; // Assuming you want to include the email
+  profilePic: string;
+  currentBalance: number;
+  numberOfNFTs: number; // Add this field for the number of NFTs a user holds
+  // Add any additional fields you want to include
+  // For example:
+  // age?: number;
+  // address?: string;
 }
+
