4fa6e34ebb962b7a9c81bd24d3c7967090b2a5ae
Emirfath5 <61555207+Emirfath5@users.noreply.github.com>
2024-01-31 15:19:15 -0500

    Update index.tsx

--- src/layouts/DefaultLayout/components/Loading/index.tsx
+++ src/layouts/DefaultLayout/components/Loading/index.tsx
@@ -1,9 +1,9 @@
-import { CircleNotch } from 'phosphor-react'
+import { skull } from 'phosphor-react'
 
 export function Loading() {
   return (
     <div className="flex h-full w-full items-center justify-center bg-background text-text">
-      <CircleNotch className="animate-spin" size="50" weight="bold" />
+      <skull className="animate-spin" size="50" weight="bold" />
     </div>
   )
 }
